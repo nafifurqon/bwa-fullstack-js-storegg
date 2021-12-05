028724725bfc9bf55adf41edb2688e66e0ffa085
nafifurqon <muhammadnafi98@gmail.com>
2021-12-05 23:07:25 +0700

    refactor: edit profile

--- components/organisms/Sidebar/MenuItem.tsx
+++ components/organisms/Sidebar/MenuItem.tsx
@@ -1,14 +1,18 @@
 import cx from 'classnames';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface MenuItemProps {
   title: string;
   icon: 'ic-menu-overview' | 'ic-menu-transaction' | 'ic-menu-messages' | 'ic-menu-card' | 'ic-menu-reward' | 'ic-menu-setting' | 'ic-menu-logout';
   active?: boolean;
+  href: string;
 }
 
 export default function MenuItem(props: Partial<MenuItemProps>) {
-  const { title, icon, active } = props;
+  const {
+    title, icon, active, href,
+  } = props;
   const classItem = cx({
     item: true,
     'mb-30': true,
@@ -21,7 +25,9 @@ export default function MenuItem(props: Partial<MenuItemProps>) {
         <Image src={`/icon/${icon}.svg`} width={25} height={25} alt="menu overview" />
       </div>
       <p className="item-title m-0">
-        <a href="" className="text-lg text-decoration-none">{title}</a>
+        <Link href={href}>
+          <a className="text-lg text-decoration-none">{title}</a>
+        </Link>
       </p>
     </div>
   );
