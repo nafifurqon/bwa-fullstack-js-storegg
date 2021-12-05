12a9cb3eac741e3105368e4c31ee030cdeefbbee
nafifurqon <muhammadnafi98@gmail.com>
2021-12-05 22:35:02 +0700

    refactor: overview content

---
+++ components/organisms/OverviewContent/TableRow.tsx
@@ -0,0 +1,64 @@
+import Image from 'next/image';
+import cx from 'classnames';
+
+interface TableRowProps {
+  title: string;
+  category: string;
+  item: number;
+  price: number;
+  status: 'Success' | 'Pending' | 'Failed';
+  image: string;
+}
+
+export default function TableRow(props: TableRowProps) {
+  const {
+    title, category, item, price, status, image,
+  } = props;
+
+  const statusClass = cx({
+    'float-start icon-status': true,
+    success: status === 'Success',
+    pending: status === 'Pending',
+    failed: status === 'Failed',
+  });
+
+  return (
+    <tr className="align-middle">
+      <th scope="row">
+        <span className="float-start me-3 mb-lg-0 mb-3">
+          <Image
+            src={`/img/${image}.png`}
+            width={80}
+            height={60}
+            alt="game thumb"
+          />
+        </span>
+        <div className="game-title-header">
+          <p className="game-title fw-medium text-start color-palette-1 m-0">
+            {title}
+          </p>
+          <p className="text-xs fw-normal text-start color-palette-2 m-0">{category}</p>
+        </div>
+      </th>
+      <td>
+        <p className="fw-medium color-palette-1 m-0">
+          {item}
+          {' '}
+          Gold
+        </p>
+      </td>
+      <td>
+        <p className="fw-medium text-start color-palette-1 m-0">{price}</p>
+      </td>
+      <td>
+        <div>
+          <span className={statusClass} />
+          <p className="fw-medium text-start color-palette-1 m-0 position-relative">
+            {status}
+
+          </p>
+        </div>
+      </td>
+    </tr>
+  );
+}
