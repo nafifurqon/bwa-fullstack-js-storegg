826ecf695dc18a44fdda6f1464b2a1c41b58c31f
nafifurqon <muhammadnafi98@gmail.com>
2022-02-05 10:31:30 +0700

    chore: sample api integration

--- components/organisms/FeaturedGame/index.tsx
+++ components/organisms/FeaturedGame/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import GameItem from '../../molecules/GameItem';
 
 export default function FeaturedGame() {
+  const [gameList, setGameList] = useState([]);
+
+  useEffect(async () => {
+    const response = await axios.get('https://nafi-bwa-storegg-server.herokuapp.com/api/v1/players/landingpage');
+    console.log('data :>> ', response.data);
+    setGameList(response.data.data);
+  }, []);
+
   return (
     <section className="featured-game pt-50 pb-50">
       <div className="container-fluid">
@@ -14,11 +24,9 @@ export default function FeaturedGame() {
           className="d-flex flex-row flex-lg-wrap overflow-setting justify-content-lg-between gap-lg-3 gap-4"
           data-aos="fade-up"
         >
-          <GameItem title="Super Mechs" category="Mobile" thumbnail="/img/Thumbnail-1.png" />
-          <GameItem title="Call of Duty: Modern" category="Mobile" thumbnail="/img/Thumbnail-2.png" />
-          <GameItem title="Mobile Legends" category="Mobile" thumbnail="/img/Thumbnail-3.png" />
-          <GameItem title="Clash of Clans" category="Mobile" thumbnail="/img/Thumbnail-4.png" />
-          <GameItem title="Valorant" category="Desktop" thumbnail="/img/Thumbnail-5.png" />
+          {gameList.map((item) => (
+            <GameItem key={item._id} title={item.name} category={item.category.name} thumbnail={`https://nafi-bwa-storegg-server.herokuapp.com/uploads/${item.thumbnail}`} />
+          ))}
         </div>
       </div>
     </section>
