d862c1e2fa81fa3aae959841f02e2f648915dc0b
nafifurqon <muhammadnafi98@gmail.com>
2022-02-08 22:46:19 +0700

    chore: get value from catgory select option

--- services/player.ts
+++ services/player.ts
@@ -20,3 +20,12 @@ export async function getDetailVoucher(id?: string) {
 
   return axiosResponse.data;
 }
+
+export async function getGameCategory() {
+  const URL = 'players/category';
+
+  const response = await axios.get(`${ROOT_API}/${API_VERSION}/${URL}`);
+  const axiosResponse = response.data;
+
+  return axiosResponse.data;
+}
