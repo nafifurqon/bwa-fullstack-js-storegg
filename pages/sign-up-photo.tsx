448d400d2a401489757785c7573828e45164ffab
nafifurqon <muhammadnafi98@gmail.com>
2022-02-11 22:20:29 +0700

    feat: created function for upload photo and sign up integration

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image';
 import { useCallback, useEffect, useState } from 'react';
+import { setSignUp } from '../services/auth';
 import { CategoryTypes } from '../services/data-types';
 import { getGameCategory } from '../services/player';
 
 export default function SignUpPhoto() {
   const [categories, setCategories] = useState([]);
   const [favorite, setFavorite] = useState('');
+  const [image, setImage] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [localForm, setLocalForm] = useState({
+    name: '',
+    email: '',
+  });
 
   const getGameCategoryAPI = useCallback(async () => {
     const data = await getGameCategory();
@@ -18,8 +25,29 @@ export default function SignUpPhoto() {
     getGameCategoryAPI();
   }, []);
 
-  const onSubmit = () => {
-    console.log('favorite :>> ', favorite);
+  useEffect(() => {
+    const getLocalForm = localStorage.getItem('user-form');
+    setLocalForm(JSON.parse(getLocalForm));
+  }, [])
+
+  const onSubmit = async () => {
+    const getLocalForm = localStorage.getItem('user-form');
+    const form = JSON.parse(getLocalForm);
+    const data = new FormData();
+    console.log('form', form)
+
+    data.append('image', image);
+    data.append('email', form.email);
+    data.append('name', form.name);
+    data.append('password', form.password);
+    data.append('username', form.name);
+    data.append('phoneNumber', '081234567890');
+    data.append('role', 'user');
+    data.append('status', 'Y');
+    data.append('favorite', favorite);
+
+    const result = await setSignUp(data);
+    console.log('result :>> ', result);
   };
 
   return (
@@ -31,13 +59,27 @@ export default function SignUpPhoto() {
               <div className="mb-20">
                 <div className="image-upload text-center">
                   <label htmlFor="avatar">
-                    <Image src="/icon/upload.svg" width={120} height={120} alt="upload" />
+                    {imagePreview ? (
+                      <img src={imagePreview} className="img-upload" alt="upload" />
+                    ) : (
+                      <Image src="/icon/upload.svg" width={120} height={120} alt="upload" />
+                    )}
                   </label>
-                  <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                  <input
+                    id="avatar"
+                    type="file"
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={(event) => {
+                      const img = event.target.files[0];
+                      setImagePreview(URL.createObjectURL(img));
+                      return setImage(img);
+                    }}
+                  />
                 </div>
               </div>
-              <h2 className="fw-bold text-xl text-center color-palette-1 m-0">Shayna Anne</h2>
-              <p className="text-lg text-center color-palette-1 m-0">shayna@anne.com</p>
+              <h2 className="fw-bold text-xl text-center color-palette-1 m-0">{localForm.name}</h2>
+              <p className="text-lg text-center color-palette-1 m-0">{localForm.email}</p>
               <div className="pt-50 pb-50">
                 <label htmlFor="category" className="form-label text-lg fw-medium color-palette-1 mb-10">
                   Favorite
