a1ac05f1e49363ce3a9c348150ac0fe491041265
nafifurqon <muhammadnafi98@gmail.com>
2022-02-12 17:48:27 +0700

    refactor: create a function to call API

--- pages/sign-up-photo.tsx
+++ pages/sign-up-photo.tsx
@@ -31,12 +31,12 @@ export default function SignUpPhoto() {
 
   useEffect(() => {
     const getLocalForm = localStorage.getItem('user-form');
-    setLocalForm(JSON.parse(getLocalForm));
+    setLocalForm(JSON.parse(getLocalForm!));
   }, []);
 
   const onSubmit = async () => {
     const getLocalForm = localStorage.getItem('user-form');
-    const form = JSON.parse(getLocalForm);
+    const form = JSON.parse(getLocalForm!);
     const data = new FormData();
 
     data.append('image', image);
@@ -51,7 +51,7 @@ export default function SignUpPhoto() {
 
     const result = await setSignUp(data);
 
-    if (result?.error === 1) {
+    if (result.error) {
       toast.error(result.message);
     } else {
       toast.success('Register berhasil');
