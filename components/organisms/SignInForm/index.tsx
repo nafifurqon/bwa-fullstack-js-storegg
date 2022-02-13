afd841658615cff3f57300a1b3a2ba61196df0be
nafifurqon <muhammadnafi98@gmail.com>
2022-02-13 13:50:40 +0700

    refactor: created global toast

--- components/organisms/SignInForm/index.tsx
+++ components/organisms/SignInForm/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import Link from 'next/link';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 import { useRouter } from 'next/router';
 import Cookies from 'js-cookie';
 import { setLogin } from '../../../services/auth';
@@ -82,7 +81,6 @@ export default function SignInForm() {
           </a>
         </Link>
       </div>
-      <ToastContainer />
     </>
   );
 }
