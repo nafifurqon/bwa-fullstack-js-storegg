6d69237c3ba67cbeff3f5af6d1e388bd619e62ae
nafifurqon <muhammadnafi98@gmail.com>
2022-02-16 21:10:35 +0700

    chore: setup form and upload image before API integration

--- pages/member/edit-profile.tsx
+++ pages/member/edit-profile.tsx
@@ -11,6 +11,7 @@ export default function EditProfile() {
     email: '',
     avatar: '',
   });
+  const [imagePreview, setImagePreview] = useState<string | null>(null);
 
   useEffect(() => {
     const token = Cookies.get('token');
@@ -27,6 +28,10 @@ export default function EditProfile() {
     }
   }, []);
 
+  const onSubmit = () => {
+    console.log('user :>> ', user);
+  };
+
   return (
     <section className="edit-profile overflow-auto">
       <Sidebar activeMenu="settings" />
@@ -38,9 +43,26 @@ export default function EditProfile() {
               <div className="photo d-flex">
                 <div className="image-upload">
                   <label htmlFor="avatar">
-                    <img src={user.avatar} alt="icon upload" width="90" height="90" style={{borderRadius: '100%' }} />
+                    {imagePreview ? (
+                      <img src={imagePreview} alt="icon upload" width="90" height="90" style={{borderRadius: '100%' }} />
+                    ) : (
+                      <img src={user.avatar} alt="icon upload" width="90" height="90" style={{borderRadius: '100%' }} />
+                    )}
                   </label>
-                  <input id="avatar" type="file" name="avatar" accept="image/png, image/jpeg" />
+                  <input
+                    id="avatar"
+                    type="file"
+                    name="avatar"
+                    accept="image/png, image/jpeg"
+                    onChange={(event) => {
+                      const img: File | null = event.target.files[0];
+                      setImagePreview(URL.createObjectURL(img));
+                      return setUser({
+                        ...user,
+                        avatar: img,
+                      });
+                    }}
+                  />
                 </div>
               </div>
               <div className="pt-30">
@@ -49,6 +71,10 @@ export default function EditProfile() {
                   type="text"
                   name="full_name"
                   value={user.name}
+                  onChange={(event) => setUser({
+                    ...user,
+                    name: event.target.value,
+                  })}
                 />
               </div>
               <div className="pt-30">
@@ -65,11 +91,11 @@ export default function EditProfile() {
               </div> */}
               <div className="button-group d-flex flex-column pt-50">
                 <button
-                  type="submit"
+                  type="button"
                   className="btn btn-save fw-medium text-lg text-white rounded-pill"
+                  onClick={onSubmit}
                 >
                   Save My Profile
-
                 </button>
               </div>
             </form>
