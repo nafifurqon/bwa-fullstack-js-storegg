f4ba635430289c0dd10da2f78e7b8e8990733343
nafifurqon <muhammadnafi98@gmail.com>
2022-02-17 09:32:08 +0700

    chore:  setup basic SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import AOS from 'aos';
+import Head from 'next/head';
 import Navbar from '../components/organisms/Navbar';
 import MainBanner from '../components/organisms/MainBanner';
 import TransactionStep from '../components/organisms/TransactionStep';
@@ -15,6 +16,14 @@ export default function Home() {
 
   return (
     <>
+      <Head>
+        <title>StoreGG - Get a New Experience in Gaming</title>
+        <meta name="description" content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati" />
+        <meta property="og:title" content="StoreGG - Get a New Experience in Gaming" />
+        <meta property="og:description" content="Kami menyediakan jutaan cara untuk membantu players menjadi pemenang sejati" />
+        <meta property="og:image" content="https://imageurlkalian" />
+        <meta property="og:url" content="https://storegg.com" />
+      </Head>
       <Navbar />
       <MainBanner />
       <TransactionStep />
